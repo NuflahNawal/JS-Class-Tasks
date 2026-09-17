64dc92f91896448b31c806aeba499426bac6c996
NuflahNawal <nuflahnawalnoor559@gmail.com>
2026-09-17 17:53:49 +0500

    Loop

---
+++ Task 3/app.js
@@ -0,0 +1,41 @@
+// var arr = [ Karachi , Lahore , Faislabad , Rawalpindi , Gujranwala , Peshawar , Multan, Hyderabad , Islamabad , Quetta , Sialkot , Bahawalpur , Sargodha , Sukkur ]
+
+// for (var i = 0; i <arr.length; i++){
+//     console.log (i , arr[i])
+// }  
+
+
+var arr = ["Karachi" , "Lahore" , "Faislabad" , "Rawalpindi" , "Gujranwala" , "Peshawar" , "Multan", "Hyderabad" , "Islamabad" , "Quetta" , "Sialkot" , "Bahawalpur" , "Sargodha" , "Sukkur" , "Larkana" , "Mardan" , "Abbottabad" , "Gwadar" ]
+
+var user = prompt ("Write a pakistan city name")
+var flag = false
+for (var i = 0; i <arr.length; i++){
+if (user == arr[i]) {
+    console.log (user = "belongs to pakistan")
+    flag = true
+}}
+if (flag == false) {
+    console.log (user = "doesn't blongs to pakistan")
+}  
+
+var arr = ["Tokyo" , "Yokohama" , "Osaka" , "Nagoya" , "Sapporo" , "Kobe" , "Kyoto" , "Fukuoka" , "Kawasaki" , "Saitama" , "Hiroshima" , "Sendai" , "Chiba" , "Kitakyushu" , "Sakai" , "Niigata" , "Hamamatsu" , "Nara"]
+
+var user = prompt ("Write a Japan city name")
+
+var flag = false 
+for (var i = 0; i <arr.length; i++){
+    if (user == arr[i]){
+        console.log (user = "belongs to Japan")
+        flag = true
+    }}
+if (flag == false) {
+    console.log (user = "doesn't belongs to Japan")
+}
+
+
+
+
+
+
+git config --global user.name "NuflahNawal"
+git config --global user.eamil "nuflahnawalnoor559@gmail.com"
