3f70aebdf7b80d3668690fa0c3639a121552e87f
NuflahNawal <nuflahnawalnoor559@gmail.com>
2026-09-22 17:57:38 +0500

    slice

---
+++ Task 5/app.js
@@ -0,0 +1,30 @@
+// for (var i = 1; i <= 5; i++){
+//     for (var j = 1; j <= 5-i; j++){
+//         document.write ("&nbsp&nbsp")
+//     }
+//     for (var k = 1; k <= i; k++){
+//         document.write ("*")
+//     }
+//     for (var l = 2; l <= i; l++){
+//         document.write ("*")
+//     }
+//     document.write ("<br>")
+// }
+
+// var movies = ("narnia", "harrypotter" , "conjuring" , "spiderman" , "ironman" , "hulk" , "thor")
+// var user = prompt ("Enter a movie name")
+// var movieTransform = user.toLowerCase()
+// console.log (user , movieTransform)
+// for (var i=0; i<movies.length; i++){
+//     if (movieTransform === movies[i]){
+//         alert ("We have this movie")
+//     }
+//     // else {"Sorry we don't have this movie"}
+// }
+
+// var months = ("january" , "faburary" , "march" )
+
+var userbd = prompt ("Enter you birth month")
+if (userbd = userbd.slice[1,3]){
+    console.log(userbd.slice[1,3])
+}
