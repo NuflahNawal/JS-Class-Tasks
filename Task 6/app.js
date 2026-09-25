a9a28c3c4510e7b739dbe59ac25942f58b8999b5
NuflahNawal <nuflahnawalnoor559@gmail.com>
2026-09-25 17:54:54 +0500

    shortkeys

---
+++ Task 6/app.js
@@ -0,0 +1,24 @@
+// var birthmounths = prompt ("Enter your Birth Month")
+// var birthmounths = birthmounths.length
+
+
+// var string = prompt ("Enter your insitute name")
+// var transform = strr.tolowercase();
+// for (var i = 0; i < transform.slice(); i++){
+//     if (transform.slice(i, i + 4)  === "SMIT" || transform.slice(i, i + 6) === "ApTech" ){
+//         console.log ("You can apply for hackathon")
+//     }
+// }
+
+// console.log (string.indexOf("smit"));
+// if (string.indexOf("smit") === -1){
+//     console.log ("You can not participate")
+// }else {
+//     console.log ("You can participate in Hackathon")
+// }
+
+var text = "Japan[a] is an island country in East Asia. Located in the Pacific Ocean off the northeast coast of the Asian mainland, it is bordered to the west by the Sea of Japan, the Sea of Okhotsk in the north, and the East China Sea in the south. The Japanese archipelago consists of four major islands alongside over 14,000 smaller islands. Japan is divided into 47 administrative prefectures and eight traditional regions, and around 75% of its terrain is mountainous and heavily forested, concentrating its agriculture and highly urbanized population along its eastern coastal plains. With a population of almost 123 million as of 2026, it is the world's 11th most populous country. Tokyo is the country's capital and largest city."
+
+var newtext = text.replace ("Japan" + "<b>" + "China")
+var newtext = text.replaceAll ("Japan" + "<b>" + "China")
+var newtext = text.replace ("Japan" + "<b>" + "China")
